9c2b6616a751ad82ce73a871611db4a0153729a0
Paul Child <pauljchild@gmail.com>
2017-01-29 15:37:40 +0000

    clean stylesheet and refactor components

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
 
 export default class MainContainer extends Component {
-
-   constructor(props) {
-      super(props)
-      // this.state = { ... }
-   }
-
-   componentDidMount() {
-   }
 
    // Toggle menu state between up and down
    handler = () => {
