109e86a3239b90b667c2d54fa4f733935b506da3
Paul Child <pauljchild@gmail.com>
2017-07-18 19:02:06 +0100

    changing product svgs

--- src/components/svg/products/MoveEducationSVG.js
+++ src/components/svg/products/MoveEducationSVG.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MoveEducationSVG = ( ) => (
    <svg version="1.1" id="Layer_1" x="0px" y="0px"
    	 width="320px" height="320px" viewBox="0 0 320 320" enable-background="new 0 0 320 320">
-   <polygon fill="#fff" points="282.196,188.216 244.573,131.784 75.423,131.784 37.804,188.216 "/>
+   <polygon fill="#fff" points="320,320 270.7,246.1 49.3,246.1 0,320 "/>
    </svg>
 )
 
