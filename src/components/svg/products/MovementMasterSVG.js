dcd2fa558488606fbd06ac9bfb77f83bf183b926
Paul Child <pauljchild@gmail.com>
2017-07-18 19:04:42 +0100

    all svgs updated

--- src/components/svg/products/MovementMasterSVG.js
+++ src/components/svg/products/MovementMasterSVG.js
@@ -4,9 +4,9 @@ const MovementMasterSVG = ( ) => (
    <svg version="1.1" id="Layer_1" x="0px" y="0px"
    	 width="320px" height="320px" viewBox="0 0 320 320" enableBackground="new 0 0 320 320">
    <g>
-   	<polygon fill="#fff" points="160,64.464 117.242,128.597 202.754,128.597 	"/>
-   	<polygon fill="#fff" points="287.382,255.536 248.163,196.711 71.834,196.711 32.618,255.536 	"/>
-   	<polygon fill="#fff" points="205.702,133.02 114.295,133.02 75.079,191.846 244.921,191.846 	"/>
+   	<polygon fill="#fff" points="160,79.2 106.3,159.7 213.7,159.7 	"/>
+   	<polygon fill="#fff" points="320,319.2 270.7,245.3 49.3,245.3 0,319.2 	"/>
+   	<polygon fill="#fff" points="217.4,165.3 102.6,165.3 53.3,239.2 266.7,239.2 	"/>
    </g>
    </svg>
 )
--- src/components/svg/products/MoverAndShakerSVG.js
+++ src/components/svg/products/MoverAndShakerSVG.js
@@ -4,8 +4,8 @@ const MoverAndShakerSVG = ( ) => (
    <svg version="1.1" id="Layer_1" x="0px" y="0px"
    	 width="320px" height="320px" viewBox="0 0 320 320" enableBackground="new 0 0 320 320">
    <g>
-   	<polygon fill="#fff" points="308.5,231.414 262.778,162.836 57.217,162.836 11.5,231.414 	"/>
-   	<polygon fill="#fff" points="213.278,88.586 106.717,88.586 61,157.164 259,157.164 	"/>
+   	<polygon fill="#fff" points="320,319.8 270.7,245.9 49.3,245.9 0,319.8 	"/>
+   	<polygon fill="#fff" points="217.4,165.9 102.6,165.9 53.3,239.8 266.7,239.8 	"/>
    </g>
    </svg>
 )
